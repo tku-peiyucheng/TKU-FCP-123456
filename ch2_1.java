5c4fa8aaab7e76e94f12e877e3559961af6ea29f
tku-peiyucheng <100709337+tku-peiyucheng@users.noreply.github.com>
2024-07-02 11:50:23 +0800

    我的第一支Java程式

---
+++ ch2_1.java
@@ -0,0 +1,14 @@
+public class ch2_1{ //定義類別 ch2_1
+    public static void main(String[] args) {
+        int num1=2;
+        int num2=3;
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cats are running");
+        System.out.println(num2+"dogs are running");
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"tigers are running");
+
+    }
+
+}
