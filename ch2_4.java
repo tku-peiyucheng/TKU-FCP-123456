6a2a13988e266951a84db15d5b8c5be3361b4a6a
tku-peiyucheng <100709337+tku-peiyucheng@users.noreply.github.com>
2024-07-03 11:37:58 +0800

    add ch2_4, ch2_5, ch2_6

---
+++ ch2_4.java
@@ -0,0 +1,10 @@
+public class ch2_4 {
+    public static void main(String[] args){
+        int num1=2;
+        int num2=3;
+
+        System.out.println("I have "+num1+" dog");
+        System.out.println("You have "+num2+ " dog");
+    }
+    
+}
---
+++ ch2_6.java
@@ -0,0 +1,9 @@
+public class ch2_6{     // 定義public類別Ex2_6
+    public static void main(String[] args){  // main()函數，主程式開始      
+        int n1=12;
+        int n2=26;
+        System.out.println(n1+"+"+n2+"="+(n1+n2));      // 印出n1+n2之值
+        System.out.println("n1+n2="+(n1+n2)); 
+        System.out.println((n1+n2)); 
+    }
+}
