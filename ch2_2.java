0c4abf900c7a48d7a00ebab609b4bbe9da29cce7
tku-peiyucheng <100709337+tku-peiyucheng@users.noreply.github.com>
2024-07-03 10:54:49 +0800

    增加 ch2_2, ch2_3

---
+++ ch2_2.java
@@ -0,0 +1,12 @@
+public class ch2_2 {
+    public static void main(String[] args){
+        int a;
+        int b=4, sum;
+        
+        a=5;
+        sum=a+b;
+        System.out.println("sum= "+ sum);
+
+    }
+    
+}
---
+++ myname.java
@@ -0,0 +1,5 @@
+public class myname{
+    public static void main(String[] args){
+        System.out.println("Pei-Yu Cheng"); 
+    }
+}
